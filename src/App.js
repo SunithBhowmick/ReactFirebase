e118b1dd3351bc64b7d747e0fd588d53be92f036
SunithBhowmick <sunith192@gmail.com>
2021-07-31 20:08:31 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import UserForm from './components/UserForm';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <UserForm />
+      </div>
+    );
+  }
+}
+
+export default App;
